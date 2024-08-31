4ff1c242975c413af9743ebad8c6e1d6b1bd74a9
bananenman <davidryngin361@gmail.com>
2024-08-31 11:24:19 +0200

    deploy

--- pages/index.vue
+++ pages/index.vue
@@ -20,9 +20,9 @@
                 <a href="#" aria-label="Visit my Github Profile" class="dropbtn"><img src="/MenuPng.webp" alt="Dropdown Button" @click="myFunction()" class="drop__button"></a>
                 <div class="dropdown-content" id="Dropdown">
                   <a href="/">Home</a>
-                  <a href="https://www.example.com/">About Egypt</a>
-                    <a href="https://www.example.com/">Prices</a>
-                    <a href="https://www.example.com/">Routes</a>
+                  <a href="/">About Egypt</a>
+                    <a href="/">Prices</a>
+                    <a href="/">Routes</a>
                     <a href="mailto:bananenmaninquiries@gmail.com" aria-label="Contact us via Mail" class="cta-drop-down">Contact Us</a>
                 </div>
               </li>
@@ -115,17 +115,17 @@
     <footer>
       <div class="footerContainer">
           <div class="socialIcons">
-            <a href="https://www.example.com/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-github"></i></a>
-            <a href="https://www.example.com/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-facebook"></i></a>
-            <a href="https://www.example.com/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-twitter"></i></a>
-            <a href="https://www.example.com/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-pinterest-alt"></i></a>
-            <a href="https://www.example.com/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-instagram"></i></a>
+            <a href="/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-github"></i></a>
+            <a href="/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-facebook"></i></a>
+            <a href="/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-twitter"></i></a>
+            <a href="/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-pinterest-alt"></i></a>
+            <a href="/" aria-label="Visit my Github Profile" @click="GithubRedirect()"><i class="bx bxl-instagram"></i></a>
           </div>
           <div class="footerNav">
               <ul>
-                  <li><a href="https://www.example.com/">About Egypt</a></li>
-                  <li><a href="https://www.example.com/">Prices</a></li>
-                  <li><a href="https://www.example.com/">Routes</a></li>
+                  <li><a href="/">About Egypt</a></li>
+                  <li><a href="/">Prices</a></li>
+                  <li><a href="/">Routes</a></li>
                   <li><a href="mailto:bananenmaninquiries@gmail.com">Contact Us</a></li>
               </ul>
           </div>
