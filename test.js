5f0697e362ae9413bd8fad7169d508dfe0c9cf49
bananenman <102436724+bananenman@users.noreply.github.com>
2024-12-12 10:09:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const storedHashedPassword = '';
+const storedHashedPassword = '$2b$10$FJw5bUXZr4EfEyRGX5k4k.i4rcGf92HzB65PCmdgb.JVjwszVZLI2';
 const userInputPassword = 'myPasswordIsSafeNow9';
 
 bcrypt.compare(userInputPassword, storedHashedPassword, (err, result) => {
