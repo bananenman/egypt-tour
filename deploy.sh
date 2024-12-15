3fcf2005c5d90e4952a292848c39d6933161d165
bananenman <davidryngin361@gmail.com>
2024-12-15 11:07:06 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ git init
 git add -A
 git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://BananaCode4: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@github.com/BananaCode4/egypt-tour.git main
+git push -f https://bananenman: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@github.com/bananenman/egypt-tour.git main
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f https://bananenman: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@github.com/bananenman/bananenman.github.io.git --branch=gh-pages gh-pages
 
