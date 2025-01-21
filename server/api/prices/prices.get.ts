f19a522aa5c9f88a9ee5456512577dff9db4ec5a
bananenman <102436724+bananenman@users.noreply.github.com>
2025-01-21 18:36:41 +0000

    Prices

--- server/api/prices/prices.get.ts
+++ server/api/prices/prices.get.ts
@@ -1,5 +1,17 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI || "" 
+const client = new MongoClient(uri);
+
 export default defineEventHandler(async (event) => {
 
-    
+    const body = await readBody<{ name: string }>(event);
+    const { name } = body;
 
+    const database = client.db("EgyTours")
+    const prices = database.collection("Prices")
+
+    const data = await prices.findOne({
+        name: name,
+    })
 });
